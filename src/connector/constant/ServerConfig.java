2e14b412de7a5bd2a560cfe2c2318ab6d80edd69
shterneregen <shterneregen@gmail.com>
2017-03-30 02:21:14 +0300

    commit Server

---
+++ src/connector/constant/ServerConfig.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connector.constant;
+
+/**
+ * Определяет сервер запущен из клиента или один
+ *
+ * @author Yura
+ */
+public class ServerConfig {
+    public static int SERVER_FROM_CLIENT = 1;   // Сервер запущен из клиента
+    public static int ONLY_SERVER = 0;          // Сервер запущен один
+}
---
+++ src/connector/constant/TrayType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connector.constant;
+
+/**
+ * Определяет тип трея
+ *
+ * @author Yura
+ */
+public class TrayType {
+    public static int SERVER_TRAY = 1;  // Трей сервера
+    public static int CLIENT_TRAY = 0;  // Трей клиента
+}
